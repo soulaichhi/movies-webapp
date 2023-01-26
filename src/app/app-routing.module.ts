a47b77491d3585f45404898490db6814b7c3da2a
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-26 15:35:25 +0100

    Create Home & Movies Pages and configuring the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {MoviesComponent} from "./pages/movies/movies.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "movies", component: MoviesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
