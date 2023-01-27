6148f9fbc61389eeeb9d381d366a4682c1c67c47
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-27 19:40:18 +0100

    Adding Animations to the slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import {HttpClientModule} from "@angular/common/http";
 import { SliderComponent } from './components/slider/slider.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SliderComponent } from './components/slider/slider.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,11 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Movie} from "../../models/movie.model";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({opacity: 0})),
+      transition('void <=> *', [animate('1s')])
+    ])
+  ]
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
   @Input() items: Movie[] = [];
+  currentSlideIndex: number = 0;
+  ngOnInit() {
+    setInterval(() => {
+      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    }, 5000);
+  }
 }
