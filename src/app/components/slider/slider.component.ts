10a615f1db9c290cdcdd3ab1da83d64510bde826
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-27 17:39:27 +0100

    Creating the Movie Model & Filling the slider with the Data from API

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Movie} from "../../models/movie.model";
 
 @Component({
   selector: 'slider',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
-
+  @Input() items: Movie[] = [];
 }
