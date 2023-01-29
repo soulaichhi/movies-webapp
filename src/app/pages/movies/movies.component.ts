de8aa349281fe2b70bae0adcda816a2a0d8497c5
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-29 20:53:18 +0100

    Create the Movies Page Layout

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "../../models/movie.model";
+import {MoviesService} from "../../services/movies.service";
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit{
+  movies: Movie[] = [];
+  constructor(private moviesService: MoviesService) {
+  }
+  ngOnInit(): void {
+    this.moviesService.getMovies('popular', 20)
+      .subscribe(movies => {
+        this.movies = movies;
+      })
+  }
 
 }
