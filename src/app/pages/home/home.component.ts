8666a559517d1b84f387bf3b289a8bd524378b6b
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-29 20:39:25 +0100

    Adjust the services to return specific number of movies

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,20 +16,20 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void {
     this.moviesService.getMovies('popular')
       .subscribe(
-        (response) => {
-          this.popularMovies = response.results;
+        (movies) => {
+          this.popularMovies = movies;
         }
       );
     this.moviesService.getMovies('upcoming')
       .subscribe(
-        (response) => {
-          this.upcomingMovies = response.results;
+        (movies) => {
+          this.upcomingMovies = movies;
         }
       );
     this.moviesService.getMovies('top_rated')
       .subscribe(
-        (response) => {
-          this.topRatedMovies = response.results;
+        (movies) => {
+          this.topRatedMovies = movies;
         }
       );
   }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {MovieDto} from "../models/movie.model";
+import {of, switchMap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,11 @@ export class MoviesService {
   baseUrl: string = 'https://api.themoviedb.org/3';
   apiKey: string = 'bfec547de689d181a4f15b9adac2d9b4';
   constructor(private http: HttpClient) { }
-  getMovies(type: string = 'upcoming') {
+  getMovies(type: string = 'upcoming', count: number = 12) {
     return this.http.get<MovieDto>(
-      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`
-      );
+      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`)
+      .pipe(switchMap( (res) => {
+        return of(res.results.slice(0, count));
+      }));
   }
 }
