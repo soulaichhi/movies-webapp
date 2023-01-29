74163a091b0c154200961ea27d9f56fad6f7d9cd
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-29 13:21:55 +0100

    Get the required services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MoviesService} from "../../services/movies.service";
+import {Movie} from "../../models/movie.model";
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,28 @@ import {MoviesService} from "../../services/movies.service";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  movies: any = [];
+  popularMovies: Movie[] = [];
+  upcomingMovies: Movie[] = [];
+  topRatedMovies: Movie[] = [];
   constructor(private moviesService: MoviesService) {
   }
   ngOnInit(): void {
-    this.moviesService.getMovies()
+    this.moviesService.getMovies('popular')
       .subscribe(
         (response:any) => {
-          this.movies = response.results;
-          console.log(this.movies);
+          this.popularMovies = response.results;
+        }
+      );
+    this.moviesService.getMovies('upcoming')
+      .subscribe(
+        (response:any) => {
+          this.upcomingMovies = response.results;
+        }
+      );
+    this.moviesService.getMovies('top_rated')
+      .subscribe(
+        (response:any) => {
+          this.topRatedMovies = response.results;
         }
       );
   }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -5,9 +5,12 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  apiKey: string = 'bfec547de689d181a4f15b9adac2d9b4';
   constructor(private http: HttpClient) { }
-
-  getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=bfec547de689d181a4f15b9adac2d9b4');
+  getMovies(type: string = 'upcoming') {
+    return this.http.get(
+      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`
+      );
   }
 }
