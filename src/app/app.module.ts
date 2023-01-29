561728b180028f4a93c7351b5f9f7a7d08b74eba
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-29 22:55:43 +0100

    Adding the Movie Detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 import { ItemComponent } from './components/item/item.component';
 import {PaginatorModule} from "primeng/paginator";
+import { MovieComponent } from './pages/movie/movie.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {PaginatorModule} from "primeng/paginator";
     MoviesComponent,
     SliderComponent,
     ItemsBannerComponent,
-    ItemComponent
+    ItemComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
