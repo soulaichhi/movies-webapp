58cd76930c10223a8780b9d3c71b4bf7b4c38c7d
Soulaimane Chhibou <Chhibou.Soulaimane@gmail.com>
2023-01-31 14:48:10 +0100

    Never use a method inside template

--- src/app/components/video-embed/video-embed.component.ts
+++ src/app/components/video-embed/video-embed.component.ts
@@ -1,16 +1,21 @@
-import {Component, Input} from '@angular/core';
-import {DomSanitizer} from "@angular/platform-browser";
+import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'video-embed',
   templateUrl: './video-embed.component.html',
   styleUrls: ['./video-embed.component.scss']
 })
-export class VideoEmbedComponent {
+export class VideoEmbedComponent implements OnInit{
   @Input() site: string = 'Youtube';
   @Input() key!: string;
+  videoUrl!: SafeResourceUrl; //never use a method inside a template. It causes multiple refresh during the lifeCylce
   constructor(private sanitizer: DomSanitizer) {
   }
+  ngOnInit(): void {
+    this.videoUrl = this.getSafeUrl('https://www.youtube.com/embed/' + this.key);
+  }
+
   getSafeUrl(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
